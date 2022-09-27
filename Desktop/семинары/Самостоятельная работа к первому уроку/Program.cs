e5724ddee991120976d235b69fc10fabd14dd29c
Andrew PV <112729614+open230884@users.noreply.github.com>
2022-09-27 21:44:42 +0300

    рабочий вариант

--- Desktop/семинары/Самостоятельная работа к первому уроку/Program.cs
+++ Desktop/семинары/Самостоятельная работа к первому уроку/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число");
+
+int n1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+
+int n2 = int.Parse(Console.ReadLine());
+
+int pow = Convert.ToInt32(Math.Pow(n2, 2));
+
+if (n1 == pow) 
+{
+    Console.WriteLine($"число {n1} является квадратом числа {n2}");
+}
+    else
+
+
+Console.WriteLine($"число {n1} не является квадратом числа {n2}");
